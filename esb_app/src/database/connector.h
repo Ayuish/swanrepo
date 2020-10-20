07652dfe429b354bc41ff4fc26a50680046122ae
KRajashekhar <2016eeb1081@iitrpr.ac.in>
2020-10-20 21:58:04 +0530

    insert_into_esb_request

---
+++ esb_app/src/database/connector.h
@@ -0,0 +1,27 @@
+//
+// Created by rajashekhar on 20/10/20.
+//
+
+#ifndef DATABASE_CONNECTOR_H
+#define DATABASE_CONNECTOR_H
+
+#include <stdio.h>
+#include <mysql/mysql.h>
+
+
+#define HOST "localhost"   /*hostname*/
+#define USER  "raja"       /*username*/
+#define PASS    "Kucharla@1"         /* Left blank for Code portability */
+#define DB_NAME "esb_db"   /*name of the database*/
+#define PORT  3306         /*port number*/
+#define UNIX_SOCKET NULL   /*unix socket*/
+#define FLAG 0            /*last parameter to mysql_real_connect */
+
+
+int connect_to_db(void);
+int finish_with_error(MYSQL *con);
+int insert_to_esb_request(char *sender_id, char *dest_id,
+                          char *message_type, char *reference_id, char *message_id,
+                          char *data_location, char *status, char *status_details, char *received_on);
+
+#endif //DATABASE_CONNECTOR_H
---
+++ esb_app/src/database/main.c
@@ -0,0 +1,10 @@
+
+#include "connector.h"
+int main() {
+    char *s,*d,*mt,*rid,*mid,*dl,*st,*std;
+    s="sender4"; d = "dest3"; mt = "CreditReports"; mid = "2";
+    dl = "dat_loc"; st ="Active"; std="process";
+    rid = "ref_id1";
+    insert_to_esb_request(s,d,mt,rid,mid,dl,st,std,"2020-08-12T05:18:00+00001");
+    return 0;
+}
