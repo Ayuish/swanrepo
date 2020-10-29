a70f4f50b210adfe48c648ff47ab2447bb4eeb14
Vinay-Prb <vinayprabhakar91@gmail.com>
2020-10-29 14:51:15 +0530

    email service completed

--- esb_app/src/adapter/adapter.h
+++ esb_app/src/adapter/adapter.h
@@ -3,10 +3,10 @@
 #define ADAPTER_H
 
 
-void *  convert_to_json(void* payload, void * name);
-void *  ftp_upload(void *asptr, void *fileptr);
-void * remove_file(void* ptr,void * ptr1) ;
-
+void*  convert_to_json(void* payload, void * name);
+void*  ftp_upload(void *asptr, void *fileptr);
+void* remove_file(void* ptr,void * ptr1) ;
+void* send_mail(void* toptr,void* file_path_ptr);
 
 
 #endif
